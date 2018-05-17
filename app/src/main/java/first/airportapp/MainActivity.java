dc2c91825117e2653e7eda754d664869a0d207ed
jonLicari <jlicari@ryerson.ca>
2018-05-17 14:02:17 -0400

    Added Firebase Web API key to google-services.json

--- app/src/main/java/first/airportapp/MainActivity.java
+++ app/src/main/java/first/airportapp/MainActivity.java
@@ -1,7 +1,13 @@
 package first.airportapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
